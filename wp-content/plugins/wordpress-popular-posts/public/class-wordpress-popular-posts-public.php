53fac971ae691344efeaf102353c268c7d6fba9a
sabrinaleroy <leroy.sabrina11@hotmail.fr>
2018-08-13 22:03:37 +0200

    Trying Disqus

--- wp-content/plugins/wordpress-popular-posts/public/class-wordpress-popular-posts-public.php
+++ wp-content/plugins/wordpress-popular-posts/public/class-wordpress-popular-posts-public.php
@@ -104,16 +104,13 @@ class WPP_Public {
 
         wp_register_script( 'wpp-js', plugin_dir_url( __FILE__ ) . 'js/wpp-4.1.0.min.js', array(), $this->version, false );
 
-        $wp_rest_api = class_exists( 'WP_REST_Controller', false );
-
         $params = array(
-            'rest_api' => (int) $wp_rest_api,
             'sampling_active' => (int) $this->admin_options['tools']['sampling']['active'],
             'sampling_rate' => $this->admin_options['tools']['sampling']['rate'],
             'ajax_url' => esc_url_raw( rest_url( 'wordpress-popular-posts/v1/popular-posts/' ) ),
             'action' => 'update_views_ajax',
             'ID' => $is_single,
-            'token' => ( $wp_rest_api ) ? wp_create_nonce( 'wp_rest' ) : wp_create_nonce( 'wpp-token' ),
+            'token' => wp_create_nonce( 'wp_rest' ),
             'debug' => WP_DEBUG
         );
         wp_localize_script( 'wpp-js', 'wpp_params', $params );
@@ -376,9 +373,6 @@ class WPP_Public {
         if ( empty( $ids ) ) {
             $shortcode_ops['pid'] = '';
         }
-        else {
-            $instance['pid'] = implode( ",", $ids );
-        }
 
         // Category filter
         $ids = array_filter( explode( ",", $shortcode_ops['cat'] ), 'is_numeric' );
@@ -386,18 +380,12 @@ class WPP_Public {
         if ( empty( $ids ) ) {
             $shortcode_ops['cat'] = '';
         }
-        else {
-            $instance['cat'] = implode( ",", $ids );
-        }
 
         // Author filter
         $ids = array_filter( explode( ",", $shortcode_ops['author'] ), 'is_numeric' );
         // Got no valid IDs, clear
         if ( empty( $ids ) ) {
             $shortcode_ops['author'] = '';
-        }
-        else {
-            $instance['author'] = implode( ",", $ids );
         }
 
         $shortcode_content = '';
@@ -416,62 +404,22 @@ class WPP_Public {
         // Return cached results
         if ( $this->admin_options['tools']['cache']['active'] ) {
 
-            $transient_name = md5( json_encode($shortcode_ops) );
-            $popular_posts = get_transient( $transient_name );
+            $key = md5( json_encode($shortcode_ops) );
+            $popular_posts = WPP_Cache::get( $key );
 
             if ( false === $popular_posts ) {
 
                 $popular_posts = new WPP_Query( $shortcode_ops );
 
-                switch( $this->admin_options['tools']['cache']['interval']['time'] ){
+                $time_value = $this->admin_options['tools']['cache']['interval']['value']; // eg. 5
+                $time_unit = $this->admin_options['tools']['cache']['interval']['time']; // eg. 'minute'
 
-                    case 'minute':
-                        $time = 60;
-                    break;
-
-                    case 'hour':
-                        $time = 60 * 60;
-                    break;
-
-                    case 'day':
-                        $time = 60 * 60 * 24;
-                    break;
-
-                    case 'week':
-                        $time = 60 * 60 * 24 * 7;
-                    break;
-
-                    case 'month':
-                        $time = 60 * 60 * 24 * 30;
-                    break;
-
-                    case 'year':
-                        $time = 60 * 60 * 24 * 365;
-                    break;
-
-                    default:
-                        $time = 60 * 60;
-                    break;
-
-                }
-
-                $expiration = $time * $this->admin_options['tools']['cache']['interval']['value'];
-
-                // Store transient
-                set_transient( $transient_name, $popular_posts, $expiration );
-
-                // Store transient in WPP transients array for garbage collection
-                $wpp_transients = get_option('wpp_transients');
-
-                if ( !$wpp_transients ) {
-                    $wpp_transients = array( $transient_name );
-                    add_option( 'wpp_transients', $wpp_transients );
-                } else {
-                    if ( !in_array($transient_name, $wpp_transients) ) {
-                        $wpp_transients[] = $transient_name;
-                        update_option( 'wpp_transients', $wpp_transients );
-                    }
-                }
+                WPP_Cache::set(
+                    $key,
+                    $popular_posts,
+                    $time_value,
+                    $time_unit
+                );
 
             }
 
